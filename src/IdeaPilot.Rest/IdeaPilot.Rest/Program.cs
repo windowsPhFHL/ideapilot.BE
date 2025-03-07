b48cd99ac03bd11baea50abff01ce167a8f03ffe
Austinstevesk <austinstevesk#gmail.com>
2025-03-07 11:48:42 +0300

    enable swagger on live site

--- src/IdeaPilot.Rest/IdeaPilot.Rest/Program.cs
+++ src/IdeaPilot.Rest/IdeaPilot.Rest/Program.cs
@@ -103,7 +103,7 @@ public class Program
             app.UseSwaggerUI();
         }
 
-            app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
         app.UseAuthorization();
         app.UseRouting();
