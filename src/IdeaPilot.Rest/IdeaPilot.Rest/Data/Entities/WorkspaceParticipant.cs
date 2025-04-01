4225c52e2fe3184d99ad1525eecebfca8e1180a5
Austinstevesk <austinstevesk#gmail.com>
2025-04-01 12:38:11 +0300

    update workspace participant entity to include prefix Workspace_participant

--- src/IdeaPilot.Rest/IdeaPilot.Rest/Data/Entities/WorkspaceParticipant.cs
+++ src/IdeaPilot.Rest/IdeaPilot.Rest/Data/Entities/WorkspaceParticipant.cs
@@ -3,7 +3,7 @@
     public sealed class WorkspaceParticipant: BaseDocument
     {
         //add properties: id, workspaceId, userId, role, status
-        public string id { get; set; } = Guid.NewGuid().ToString();
+        public string id { get; set; } = Guid.NewGuid().ToString("N").Insert(0, "Workspace_participant_");
         public string WorkspaceId { get; set; }
         public string UserId { get; set; }
         public string Role { get; set; } = string.Empty;
